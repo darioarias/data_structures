1864d258dc8063aad2b448851de5aa373bbed82a
Dario <darioarias12@gmail.com>
2022-12-17 06:38:15 -0500

    boilerplate for testing avl adt node

---
+++ tests/unit/test_avl_node.py
@@ -0,0 +1,12 @@
+import operator
+import unittest
+
+from data_structures.nodes import AVLTreeNode as Node
+
+
+class TestAVLNode(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
