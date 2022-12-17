a00452780b131bdea3ebf65252b16afe2ef65b49
Dario <darioarias12@gmail.com>
2022-12-16 19:00:02 -0500

    boilerplate for testing Binary Search Tree abstract data type (node).

---
+++ tests/unit/test_bst_node.py
@@ -0,0 +1,12 @@
+import operator
+import unittest
+
+from data_structures.nodes import BinarySearchTreeNode as Node
+
+
+class TestBSTNode(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
