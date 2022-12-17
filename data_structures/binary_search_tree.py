b786293cd6de0ca5b51b9bc4365e464801921191
Dario <darioarias12@gmail.com>
2022-12-16 21:17:02 -0500

    defines how to inserts data into the bst data structure.

--- data_structures/binary_search_tree.py
+++ data_structures/binary_search_tree.py
@@ -14,5 +14,19 @@ class BinarySearchTree(Generic[CT]):
             self.root = self.from_iter(__items).root
             return None
 
+    def insert(self, value: CT) -> None:
+        """Inserts a value into the tree"""
+
+        def insert_helper(root: Optional[Node[CT]], value: CT) -> Node[CT]:
+            if root is None:
+                return Node[CT](value)
+            if value < root:
+                root.left = insert_helper(root.left, value)
+            else:
+                root.right = insert_helper(root.right, value)
+            return root
+
+        self.root = insert_helper(self.root, value)
+
 
 __all__ = ["BinarySearchTree"]
