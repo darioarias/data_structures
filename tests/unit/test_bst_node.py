ee0a4fb3f57b63306edc607057ee8a6e251f7b06
Dario <darioarias12@gmail.com>
2022-12-16 19:03:59 -0500

    setting up the class for the test. Node the values that are dynamically created.

--- tests/unit/test_bst_node.py
+++ tests/unit/test_bst_node.py
@@ -5,7 +5,13 @@ from data_structures.nodes import BinarySearchTreeNode as Node
 
 
 class TestBSTNode(unittest.TestCase):
-    pass
+    def setUp(self) -> None:
+        self.vals = (1, 2, 3)
+        self.nodes = (Node(val) for val in self.vals)
+
+    def tearDown(self) -> None:
+        del self.vals
+        del self.nodes
 
 
 if __name__ == "__main__":
