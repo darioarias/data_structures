ec3389ccf0467303c26efff8ecde1318d9281ffc
Dario <darioarias12@gmail.com>
2022-12-16 21:19:12 -0500

    overloading the str diagram to represent a tree on the console.

--- data_structures/binary_search_tree.py
+++ data_structures/binary_search_tree.py
@@ -65,5 +65,23 @@ class BinarySearchTree(Generic[CT]):
 
         return contains(self.root, item)
 
+    def __str__(self) -> str:
+        def diagram(
+            node: Optional[Node[CT]], top: str = "", root: str = "", bottom: str = ""
+        ) -> str:
+            """This algorithm is based on an implementation by Károly Lőrentey in his book [Optimizing Collections](https://www.objc.io/books/optimizing-collections/)"""
+            if node is None:
+                return root + "None\n"
+            if node.left is None and node.right is None:
+                return root + f"{node.value!r}\n"
+            return (
+                diagram(node.right, top + "  ", top + "┌─", top + "│ ")
+                + root
+                + f"{node.value!r}\n"
+                + diagram(node.left, bottom + "│ ", bottom + "└─", bottom + "  ")
+            )
+
+        return diagram(self.root)
+
 
 __all__ = ["BinarySearchTree"]
