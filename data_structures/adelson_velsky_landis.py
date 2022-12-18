31f688076600ddf2fdf13a0ec71c524666f98d80
Dario <darioarias12@gmail.com>
2022-12-18 13:43:17 -0500

    Implementing the insert method. Note the inner helper function which does all of the heavy lifting in a recursive manner. Moreover, note that the inner helper function also makes use of another class-level helper.

--- data_structures/adelson_velsky_landis.py
+++ data_structures/adelson_velsky_landis.py
@@ -13,5 +13,21 @@ class AVL(Generic[CT]):
         if __items is not None:
             self.root = self.from_iter(__items).root
 
+    def insert(self, value: CT) -> None:
+        def insert_helper(node: Optional[Node[CT]], value: CT) -> Node[CT]:
+            if node is None:
+                return Node(value)
+
+            if value < node:
+                node.left = insert_helper(node.left, value)
+            else:
+                node.right = insert_helper(node.right, value)
+
+            balanced = self._balanced(node)
+            balanced.height = max(balanced.left_height, balanced.right_height) + 1
+            return balanced
+
+        self.root = insert_helper(self.root, value)
+
 
 __all__ = ["AVL"]
