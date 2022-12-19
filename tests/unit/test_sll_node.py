1a64fef2cfc53dfb17731ce472550905a9dff359
Dario <darioarias12@gmail.com>
2022-12-19 06:14:56 -0500

    Creating default values to test with. These are removed after each test.

--- tests/unit/test_sll_node.py
+++ tests/unit/test_sll_node.py
@@ -5,10 +5,12 @@ from data_structures.nodes import SinglyLinkedListNode as Node
 
 class TestSLLNode(unittest.TestCase):
     def setUp(self) -> None:
-        ...
+        self.vals = (1, 2, 3)
+        self.nodes = (Node(val) for val in self.vals)
 
     def tearDown(self) -> None:
-        ...
+        del self.vals
+        del self.nodes
 
 
 if __name__ == "__main__":
