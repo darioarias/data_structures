e343fba486cff90254273f985832331027d4e3ce
Dario <darioarias12@gmail.com>
2022-12-19 06:34:05 -0500

    boilerplate for singly linked lists implementation

---
+++ data_structures/linked_lists.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+import typing
+
+from .nodes import SinglyLinkedListNode as SLLNode
+
+T = typing.TypeVar("T")
+
+
+class SinglyLinkedList(typing.Generic[T]):
+    pass
+
+
+__all__ = ["SinglyLinkedList"]
