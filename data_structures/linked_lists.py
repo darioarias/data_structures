e2ec9b1a1bd93c2ad6514d33c80821056249d2e3
Dario <darioarias12@gmail.com>
2022-12-19 06:35:16 -0500

    append method: adding data

--- data_structures/linked_lists.py
+++ data_structures/linked_lists.py
@@ -23,5 +23,13 @@ class SinglyLinkedList(typing.Generic[T]):
         if self._tail is None:
             self._tail = self._head
 
+    def append(self, value: T) -> None:
+        """Adds a value at the end of the list."""
+        if self._tail is None:
+            return self.push(value)
+
+        self._tail.next = SLLNode[T](value)
+        self._tail = self._tail.next
+
 
 __all__ = ["SinglyLinkedList"]
