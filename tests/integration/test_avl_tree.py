49c6794b0037c1482268256238b1ba3dfdf79174
Dario <darioarias12@gmail.com>
2022-12-18 19:48:38 -0500

    boilerplate for avl tree tests.

---
+++ tests/integration/test_avl_tree.py
@@ -0,0 +1,13 @@
+import operator
+import unittest
+
+from data_structures.adelson_velsky_landis import AVL as Tree
+from data_structures.nodes import AVLTreeNode as TreeNode
+
+
+class TestAVLTree(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
