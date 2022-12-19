edfe23df1ed8e92b7b995691b56f722526c4e97a
Dario <darioarias12@gmail.com>
2022-12-19 06:13:00 -0500

    boilerplate for testing singly linked list abstract data type.

---
+++ tests/unit/test_sll_node.py
@@ -0,0 +1,15 @@
+import unittest
+
+from data_structures.nodes import SinglyLinkedListNode as Node
+
+
+class TestSLLNode(unittest.TestCase):
+    def setUp(self) -> None:
+        ...
+
+    def tearDown(self) -> None:
+        ...
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
