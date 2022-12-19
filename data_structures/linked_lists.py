b301e518217d8d2e0a4b4e54b8b24f5b5d0f410c
Dario <darioarias12@gmail.com>
2022-12-19 06:34:30 -0500

    class constructor

--- data_structures/linked_lists.py
+++ data_structures/linked_lists.py
@@ -8,7 +8,13 @@ T = typing.TypeVar("T")
 
 
 class SinglyLinkedList(typing.Generic[T]):
-    pass
+    def __init__(self, __items: typing.Optional[typing.Iterable[T]] = None) -> None:
+        self._head: typing.Optional[SLLNode[T]] = None
+        self._tail: typing.Optional[SLLNode[T]] = None
+
+        if __items is not None:
+            for item in __items:
+                self.append(item)
 
 
 __all__ = ["SinglyLinkedList"]
