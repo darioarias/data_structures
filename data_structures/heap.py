d379c479a8e7db952384c03e06addfff7e73e478
Dario <darioarias12@gmail.com>
2022-12-20 07:04:15 -0500

    boilerplate for Heap class.

---
+++ data_structures/heap.py
@@ -0,0 +1,12 @@
+import operator
+import reprlib
+from typing import Callable, Generic, Iterator, Optional
+
+from ._protocols import CT
+
+
+class Heap(Generic[CT]):
+    pass
+
+
+__all__ = ["Heap"]
