b6ccec0c5ec5c52b0cac2df38e5db71e761f795e
Dario <darioarias12@gmail.com>
2022-12-21 08:47:50 -0500

    Default sorting function for the queue. operator.lt means that lower values will have higher priority.

--- data_structures/priority_queue.py
+++ data_structures/priority_queue.py
@@ -7,6 +7,13 @@ from .heap import Heap
 _T = TypeVar("_T")
 
 
+def _default_key(a: _T, b: _T) -> bool:
+    try:
+        return operator.lt(cast(CT, a), cast(CT, b))
+    except:
+        raise TypeError(f"Key function missing")
+
+
 class PriorityQueue(Generic[_T]):
     pass
 
